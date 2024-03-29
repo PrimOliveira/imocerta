12ce1f246f95631464aa2168d6a3c8258337120e
PrimOliveira <145802403+PrimOliveira@users.noreply.github.com>
2024-03-29 21:02:26 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+$('.carousel').carousel({
+    interval: 5000, // Intervalo de 5 segundos entre os slides
+    pause: false // Não pausar a rotação do carrossel quando o mouse estiver sobre ele
+  });
